bf896d11adacd62be5108f6532403e5e7e59490a
Bolo <bolo@cs.wisc.edu>
2017-11-09 13:16:52 -0600

    Allow swamp-cli to be configured from environment
    Shell syntax update

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,13 +2,18 @@
 
 function main {
     #DIR_NAME="$(dirname $(dirname $0))"
-    local cli_jar="$PWD/target/swamp-cli-jar-with-dependencies.jar"
+
+    if [ -n "$SWAMP_CLI_JAR" ] ; then
+	local cli_jar="$SWAMP_CLI_JAR"
+    else
+	local cli_jar="$PWD/target/swamp-cli-jar-with-dependencies.jar"
+    fi
 
     [[ ! -f  "$cli_jar" ]] && echo "File not found: $cli_jar" && exit 1
 
     #JYTHON_JAR="$HOME/jython/jython/jython-standalone-2.7.0.jar"
 
-    if [[ ! -f  "$JYTHON_JAR" ]]; then
+    if [[ ! -f  "$JYTHON_JAR" ]] ; then
         echo "Requires Jython stand-alone Jar file, from http://www.jython.org/downloads.html download the latest Jython stand-alone jar and create an environment variable by the name JYTHON_JAR" \
         && exit 1
     fi
